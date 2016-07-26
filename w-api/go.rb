fa992e9d89f75419b4afd35063e7a3e47f451d25
Rafkat Galiullin <xiaomirus@gmail.com>
2016-07-26 09:39:15 +0000

    few fixes

--- w-api/go.rb
+++ w-api/go.rb
@@ -7,13 +7,13 @@ SERVICE_HOST = ENV['SERVICE_HOST'] || 'localhost'
 SERVICE_PORT = ENV['SERVICE_PORT'] || 8999
 
 def json_rpc_connection
-  conn = Jimson::Client.new("#{SERVICE_HOST}:#{SERVICE_PORT}") # the URL for the JSON-RPC 2.0 server to connect to
+  conn = Jimson::Client.new("#{SERVICE_HOST}:#{SERVICE_PORT}") # setup JSON-RPC 2.0 server connection
 end    
 
-get '/api', provides: [:json] do
+get '/api/v1/eta', provides: [:json] do
   conn = json_rpc_connection
   lat = Float(params[:latitude])
   lng = Float(params[:longitude])
-  result = conn.harvestine_distance(lat, lng) # call the 'sum' method on the RPC server and save the result '3'
+  result = conn.calculate_eta(lat, lng) # call the 'harvestine_distance' method on the RPC server
   {eta: result}.to_json
 end
--- w-service/service.rb
+++ w-service/service.rb
@@ -3,10 +3,10 @@ require 'jimson'
 class ServiceHandler
   extend Jimson::Handler 
 
-  def harvestine_distance(a,b)
-    rand(5..90).to_s
+  def calculate_eta(a,b)
+    rand(5..90).to_s # * 1.5
   end
 end
 
 server = Jimson::Server.new(ServiceHandler.new)
-server.start # serve with webrick on http://0.0.0.0:8999/
+server.start # serve on http://0.0.0.0:8999/
